bd3ea7fc696938b8a5b955b973330882b951fe45
Yoshua Wakeham <yoshwakeham@gmail.com>
2014-07-09 18:13:45 +1000

    added docstrings, comments, updated README, tweaked code

--- README.md
+++ README.md
@@ -1,32 +1,22 @@
-# Ulamspiral
+# Poly_area
 
 ### What is it?
 
-A simple script for generating Ulam spirals,
-which are visualisations of patterns in the prime
-numbers. A lower and upper bound for the spiral
-may be specified; these bounds determine the
-range of integers whose primality will be
-visualised. Each pixel in the generated image
-represents an integer. If the integer is prime,
-the pixel will be coloured (blue, by default).
-Otherwise, the pixel will be black.
-
-The spirals are generated as PNG images,
-using PIL (via Bernie Pope's SimpleImage.py
--- see module header for URL).
+A simple script for calculating the surface area
+of an arbitrary two-dimensional polygon, where the
+polygon is specified by a sequence of vertices,
+stored as x-y co-ordinates in a CSV file.
 
 ### Usage
 
-Ulamspiral is a command-line tool. Run it with
+Poly_area is a command-line tool. Run it with
 
-    $ python ulamspiral.py [LOWER] UPPER
+    $ python poly_area.py FILENAME 
 
-where LOWER and UPPER are both integers, with LOWER
-optional (defaulting to 1 if unspecified). If both bounds
-are supplied, UPPER must be strictly greater than LOWER.
+where FILENAME points to a csv file containing
+a sequence of x-y co-ordinates.
 
-Alternatively, `import ulamspiral` within a Python
+Alternatively, `import poly_area` within a Python
 module to access its functions.
 
 ### License
--- poly_area.py
+++ poly_area.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# ::: POLY AREA :::
+# ::: POLY_AREA :::
 #
 # AUTHOR
 #
@@ -11,9 +11,23 @@
 #
 # DATE CREATED
 #
-#    xx July 2014
+#    9 July 2014
 #
 # NOTES
+#
+#    Poly_area is a simple script for calculating the surface area
+#    of a polygon in the Cartesian (x-y) plane, when the polygon
+#    is specified by an arbitrary sequence of vertices contained in
+#    a CSV file.
+#
+#    The algorithm traverses the vertices in the given order, and
+#    uses a line integral/summation method based on Green's Theorem
+#    to compute the surface area.
+#
+#    (See https://en.wikipedia.org/wiki/Green%27s_theorem for more.)
+#
+#    Poly_area may be run as a command-line script, or imported as
+#    a standard Python module.
 #
 #    This code is designed to be run with Python 2.7x. It was
 #    written and tested on a MacBook Pro running OSX 10.9 Mavericks.
@@ -35,13 +49,19 @@
 #
 ###############################################################################
 
-import csv
-import sys
+import csv, sys
 
 
 ### FUNCTIONS #################################################################
 
 def readpoints(fname):
+    """
+    readpoints(fname) -> list of x-y co-ords stored as 2-tuples
+
+    Reads the contents of the input file, assumed to be in CSV format,
+    and provided the contents are well-formed, converts the CSV data
+    into a list of 2-tuples representing Cartesian co-ordinates.
+    """
     try:
         file = open(fname,"r")
     except IOError, e:
@@ -52,41 +72,53 @@ def readpoints(fname):
     for row in reader:
         if len(row)!=2:
             print("Error: input row must have exactly 2 entries:{}".format(row))
+            file.close()
             exit()
         try:
             points.append((float(row[0]),float(row[1])))
         except ValueError:
             print("Error: numeric conversion of input failed for file: {}."\
                     .format(fname))
+            file.close()
             exit()
+    file.close()
     return points
 
 
 def getarea(points):
     """
+    getarea(list of floating pt 2-tuples) -> surface area (float)
+
     Calculates the surface area of a polygon in
-    the x-y plane, using a reduced form of Green's Theorem.
-    Assumes that the polygon is specified by a sequence
-    of vertices, in order of counterclockwise traversal.
+    the x-y plane, using a simplified form of Green's Theorem.
+    Assumes that the input list is a sequence of vertices,
+    stored as floating point 2-tuples. The algorithm traverses
+    these vertices in a clockwise fashion.
     """
     area = 0.0
     for i in range(len(points)-1):
-        area += segmentint(points[i],points[i+1])
-    area += segmentint(points[-1],points[0])
+        area += seg_integral(points[i],points[i+1])
+    # calculate final segment, closing the polygon
+    area += seg_integral(points[-1],points[0])
     return area
 
 
-def segmentint(p1,p2):
+def seg_integral(p1,p2):
     """
-    Calculate a line integral for a line segment which,
-    when summed over a closed piecewise continuous boundary,
-    gives the surface area of the enclosed region.
+    seg_integral((float, float), (float, float)) -> integral value (float)
+
+    Calculate a line integral for the line segment with
+    endpoints p1 and p2. This is a simplified form of the
+    integral of the field (-y,x)/2 along the line. When the
+    segment integrals are summed over a closed, piecewise
+    continuous boundary, we obtain the surface area of the
+    enclosed region.
     """
     try:
         (x1,y1) = p1
         (x2,y2) = p2
     except (TypeError, ValueError):
-        print("Error: invalid input to function segmentint()")
+        print("Error: invalid input to function seg_integral()")
         exit()
     xavg = (x1 + x2) / 2
     yavg = (y1 + y2) / 2
